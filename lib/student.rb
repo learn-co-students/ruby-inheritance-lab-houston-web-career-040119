e3f672221425a9882be845fa1e684deeb93e69f1
jpavioli <justin.avioli@gmail.com>
2019-04-06 18:18:50 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(something_new)
+    @knowledge << something_new
+  end
 
 end
